4cca6e9a7243e862fddfc2ffc7d1254eaabb1966
thor <thorck@protonmail.com>
2022-07-07 15:59:53 -0700

    build-compress

---
+++ .github/scripts/check_test_fmt.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e # exit immediately if command ends with non-zero exit status.
+
+# necessary for rustfmt full features
+rustup default nightly
+rustup component add rustfmt
+
+# ✨check test fmt✨
+cargo check
+cargo test --all-features --verbose
+cargo fmt --all -- --check
