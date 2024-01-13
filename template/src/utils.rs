52c32941388f3ef9b91d130299296da5c9abcfe5
Thor Kamphefner <thorck@pm.me>
2024-01-13 14:10:21 -0800

    birb-2024-01-13-22:10UTC

--- template/src/utils.rs
+++ template/src/utils.rs
@@ -8,11 +8,11 @@ use log::{error, info};
 use crate::error::MyError;
 
 /// Set up crate logging and environment variables.
-{% if cli %}
+{% if cli -%}
 pub(crate) fn setup() -> Result<Cli, MyError> {
 {% else %}
 pub(crate) fn setup() -> Result<(), MyError> {
-{%- endif %}
+{%- endif -%}
   dotenv::dotenv().ok();
   env_logger::init();
   // tracing_init::init_tracing(); // async alternative
@@ -26,28 +26,30 @@ pub(crate) fn setup() -> Result<(), MyError> {
   {% if cli %}
   let args = cli::Cli::parse();
   Ok(args)
-  {%- endif %}
+  {% else %}
   Ok(())
+  {%- endif %}
 }
 
-mod tracing_init {
-  /// Set up the tracing filter level using the env value, or else set it here. Reads RUST_LOG.
-  /// TRACE < DEBUG < INFO < WARN < ERROR
-  #[tracing::instrument]
-  pub(crate) fn init_tracing() {
-    let filter = tracing::level_filters::LevelFilter::INFO.into();
-    // set level to RUST_LOG env variable, or else INFO
-    let filter =
-      tracing_subscriber::EnvFilter::builder().with_default_directive(filter).from_env_lossy();
-    //  .with_level(false) // don't include levels in formatted output
-    //  .with_target(false) // don't include targets
-    //  .with_thread_ids(true) // include the thread ID of the current thread
-    //  .with_thread_names(true) // include the name of the current thread
-    //  .compact(); // use the `Compact` formatting style.
-    tracing_subscriber::fmt().with_env_filter(filter).init();
-  }
-}
+// mod tracing_init {
+//   /// Set up the tracing filter level using the env value, or else set it here. Reads RUST_LOG.
+//   /// TRACE < DEBUG < INFO < WARN < ERROR
+//   #[tracing::instrument]
+//   pub(crate) fn init_tracing() {
+//     let filter = tracing::level_filters::LevelFilter::INFO.into();
+//     // set level to RUST_LOG env variable, or else INFO
+//     let filter =
+//       tracing_subscriber::EnvFilter::builder().with_default_directive(filter).from_env_lossy();
+//     //  .with_level(false) // don't include levels in formatted output
+//     //  .with_target(false) // don't include targets
+//     //  .with_thread_ids(true) // include the thread ID of the current thread
+//     //  .with_thread_names(true) // include the name of the current thread
+//     //  .compact(); // use the `Compact` formatting style.
+//     tracing_subscriber::fmt().with_env_filter(filter).init();
+//   }
+// }
 
+{% if cli %}
 mod cli {
   use clap::{Args, Parser};
 
@@ -67,8 +69,9 @@ mod cli {
     /// Name of the person to greet
     #[arg(short, long)]
     pub name: String,
-    // /// Number of times to greet
+    /// Number of times to greet
     #[arg(short, long, default_value_t = 1)]
     pub count: usize,
   }
 }
+{% endif %}
