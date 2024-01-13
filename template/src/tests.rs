2624f1c22e995739a62fb6c0b7a0252aa8546405
Thor Kamphefner <thorck@pm.me>
2024-01-13 12:10:21 -0800

    birb-2024-01-13 20:10UTC

--- template/src/tests.rs
+++ template/src/tests.rs
@@ -1,11 +1,47 @@
-use arbitrary::Arbitrary;
-use quickcheck_macros::quickcheck;
-use rstest::{fixture, rstest};
-// logs in tests, can wrap other test macros
-use test_log::test as ltest;
+mod demo {
+    use arbitrary::Arbitrary;
+    use rstest::{fixture, rstest};
+    use test_log::test as ltest; // enables logging in tests
+    use log::info;
+    // rstest provides features to take common context into tests, and set up small cases testing
+    /// Some context to take into test functions, via `rstest`
+    #[derive(Clone, Debug, Eq, PartialEq, Arbitrary)]
+    struct TestWorkbench {
+      b: bool,
+      count: usize,
+    }
+    // context setup function to be implicitly called by `workbench`
+    #[fixture]
+    fn count() -> usize { 0 }
+    // context setup function to be implicitly called by `test_workbench`
+    #[fixture]
+    fn workbench(#[default(false)] b: bool, count: usize) -> TestWorkbench { TestWorkbench { b, count } }
 
-#[test]
-fn it_works() {
-  let _a = 2usize;
-  assert_eq!(2 + 2, 4);
+    // small-cases fuzzing
+    #[rstest] // argument workbench will inherit the above function if names match; will generate 3x3 case-tests
+    #[case(0, true, true)]
+    #[case(1, true, false)]
+    #[ltest] // must follow rstest
+    fn test_workbench(workbench: TestWorkbench, #[case] n: usize, #[case] b: bool, #[case] expected: bool) {
+      info!("test logged, workbench is {workbench:?}");
+      let wb = Workbench { n, b };
+      assert_eq!(workbench == wb, expected); // this will fail for some cases
+    }
+
+    // ex 2 - baby fuzz; will generate 2x2 test cases
+    #[rstest]
+    fn test_enumerative(#[values(0, 4)] n: usize, #[values(7, 8)] m: usize) { assert!(n < m) }
+
+    // fuzz test
+    fn reverse<T: Clone>(xs: &[T]) -> Vec<T> {
+      let mut rev = vec![];
+      for x in xs.iter() {
+        rev.insert(0, x.clone())
+      }
+      rev
+    }
+
+    // fuzz, declare quickcheck on any argument implementing Arbitrary
+    #[quickcheck_macros::quickcheck]
+    fn prop(xs: Vec<u32>) -> bool { xs == reverse(&reverse(&xs)) }
 }
